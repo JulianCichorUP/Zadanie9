30a3ee68fe7e72b9a69a36133bfc6ffd9fc65638
JulianCichorUP <73111644+JulianCichorUP@users.noreply.github.com>
2020-12-09 14:35:49 +0100

    Przepisanie zgodnie z konwencją PEP8

--- Copy content.py
+++ Copy content.py
@@ -1,6 +1,6 @@
 try:
-    with open("plik1.txt",'r') as file1:
-        with open("plik2.txt",'w') as file2:
+    with open("plik1.txt", 'r') as file1:
+        with open("plik2.txt", 'w') as file2:
             for linia in file1:
                 file2.write(linia)
 except:
